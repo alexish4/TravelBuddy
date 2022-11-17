74f8a6844b5696ae98c3260fcc1ee04f17147420
alexish4 <alexish4@cpp.edu>
2022-11-17 11:06:32 -0800

    Merged Estefania's code

--- app/src/main/java/com/bignerdranch/android/travelbuddy/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/travelbuddy/MainActivity.kt
@@ -12,13 +12,21 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.json.JSONObject
+import java.math.RoundingMode
 import java.net.URL
+import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
 
     var baseCurrency = "EUR"
     var convertedToCurrency = "USD"
     var conversionRate = 0f
+
+    //Miles and kilometers
+    var baseLength = "Kilometers"
+    var convertedToLength = "Miles"
+    private lateinit var et_firstLengthConversion: EditText
+    private lateinit var et_secondLengthConversion: EditText
 
     private lateinit var et_firstCurrencyConversion: EditText
     private lateinit var et_secondCurrencyConversion: EditText
@@ -29,6 +37,10 @@ class MainActivity : AppCompatActivity() {
 
         spinnerCurrencySetup()
         textCurrencyChanged()
+
+        //Miles and kilometers
+        spinnerLengthSetup()
+        textChangedLength()
     }
 
     private fun textCurrencyChanged() {
@@ -51,6 +63,53 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
+    }
+
+    private fun textChangedLength() {
+        et_firstLengthConversion = findViewById<EditText>(R.id.et_firstLengthConversion) //find by reference
+        et_firstLengthConversion.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                Log.d("Main", "Before Text Changed")
+            }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                Log.d("Main", "On Text Changed")
+            }
+            override fun afterTextChanged(s: Editable?) {
+                try {
+                    getLengthResult()
+                } catch (e: Exception) {
+                    Log.e("Main", "$e")
+                }
+            }
+        })
+    }
+
+    private fun getLengthResult() {
+        et_firstLengthConversion = findViewById<EditText>(R.id.et_firstLengthConversion) //find by reference
+        et_secondLengthConversion = findViewById<EditText>(R.id.et_secondLengthConversion)
+        if(et_firstLengthConversion != null
+            && et_firstLengthConversion.text.isNotEmpty() && et_firstLengthConversion.text.isNotBlank()) {
+            if(baseLength == "Kilometers" && convertedToLength == "Miles"){
+                val df = DecimalFormat("#.###")
+                df.roundingMode = RoundingMode.CEILING
+                val text = df.format((et_firstLengthConversion.text.toString().toFloat() *  0.621371)).toString()
+                et_secondLengthConversion?.setText(text)
+            }
+
+            else if(baseLength == "Miles" && convertedToLength == "Kilometers"){
+                val df = DecimalFormat("#.###")
+                df.roundingMode = RoundingMode.CEILING
+                val text2 = df.format((et_firstLengthConversion.text.toString().toFloat()* 1.6093)).toString()
+                et_secondLengthConversion?.setText(text2)
+            }
+            else if(baseLength == convertedToLength) {
+                Toast.makeText(
+                    applicationContext,
+                    "Cannot convert the same length",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
     }
 
     private fun getApiResult() {
@@ -145,6 +204,56 @@ class MainActivity : AppCompatActivity() {
                 getApiResult()
             }
 
+        })
+    }
+
+    private fun spinnerLengthSetup() {
+        val spinnerlength: Spinner = findViewById(R.id.spinner_firstLengthConversion)
+        val spinnerlength2: Spinner = findViewById(R.id.spinner_secondLengthConversion)
+        et_firstLengthConversion = findViewById<EditText>(R.id.et_firstLengthConversion)
+        et_secondLengthConversion = findViewById<EditText>(R.id.et_secondLengthConversion)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.length,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinnerlength.adapter = adapter
+        }
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.length2,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinnerlength2.adapter = adapter
+        }
+        spinnerlength.onItemSelectedListener = (object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                baseLength = parent?.getItemAtPosition(position).toString()
+            }
+        })
+        spinnerlength2.onItemSelectedListener = (object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                convertedToLength = parent?.getItemAtPosition(position).toString()
+            }
         })
     }
 
